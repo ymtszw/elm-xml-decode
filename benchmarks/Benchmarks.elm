37c25ad5342a10f5bae0656cd2a7091d3ecfde2b
ymtszw <ymtszw@gmail.com>
2017-12-19 23:57:30 +0900

    [master] Bench: benchmark by w3school XML examples

--- benchmarks/Benchmarks.elm
+++ benchmarks/Benchmarks.elm
@@ -1,29 +1,82 @@
 module Benchmarks exposing (main)
 
-import Benchmark exposing (..)
+import Benchmark exposing (Benchmark, describe, benchmark)
 import Benchmark.Runner exposing (BenchmarkProgram, program)
+import ExampleXml
+import XmlParser
+import Xml.Decode as XD
+import Xml.Decode.Extra exposing ((|:))
+import Xml.Decode.Internal as Internal
 
 
 suite : Benchmark
 suite =
-    let
-        longString =
-            String.repeat 100 "abcdefgh"
-
-        mapper char =
-            case char of
-                'd' ->
-                    'D'
-
-                _ ->
-                    char
-    in
-        describe "String"
-            [ describe "map"
-                [ benchmark "replace characters in a long string" <|
-                    \_ -> String.map mapper longString
-                ]
+    describe "Components of elm-xml-decode"
+        [ describe "Parsing minimal XML"
+            [ benchmark "elm-xml-parser (using elm-tools/parser)" <|
+                \_ -> XmlParser.parse ExampleXml.justRoot
             ]
+        , Benchmark.compare "Escape CDATA for error dump"
+            "escape (char-based)"
+            (\_ -> Internal.escape escapableString)
+            "escapeR (regex-based)"
+            (\_ -> Internal.escapeR escapableString)
+        , describe "Decode actual XmlParser.Xml"
+            [ benchmark "decode note.xml (small)" <|
+                \_ -> XD.decodeXml noteDecoder noteXml
+            , benchmark "decode cd_catalog.xml (large)" <|
+                \_ -> XD.decodeXml (XD.path [ "CD" ] (XD.list cdCatalogDecoder)) cdCatalogXml
+            ]
+        ]
+
+
+escapableString : String
+escapableString =
+    " '<ab&cd>' "
+
+
+noteDecoder : XD.Decoder ( String, String, String, String )
+noteDecoder =
+    XD.succeed (,,,)
+        |: XD.path [ "to" ] (XD.single XD.string)
+        |: XD.path [ "from" ] (XD.single XD.string)
+        |: XD.path [ "heading" ] (XD.single XD.string)
+        |: XD.path [ "body" ] (XD.single XD.string)
+
+
+noteXml : XmlParser.Xml
+noteXml =
+    parse ExampleXml.note
+
+
+type alias CD =
+    ( String, String, String, String, Float, Int )
+
+
+cdCatalogDecoder : XD.Decoder CD
+cdCatalogDecoder =
+    XD.succeed (,,,,,)
+        |: XD.path [ "TITLE" ] (XD.single XD.string)
+        |: XD.path [ "ARTIST" ] (XD.single XD.string)
+        |: XD.path [ "COUNTRY" ] (XD.single XD.string)
+        |: XD.path [ "COMPANY" ] (XD.single XD.string)
+        |: XD.path [ "PRICE" ] (XD.single XD.float)
+        |: XD.path [ "YEAR" ] (XD.single XD.int)
+
+
+cdCatalogXml : XmlParser.Xml
+cdCatalogXml =
+    parse ExampleXml.cdCatalog
+
+
+parse : String -> XmlParser.Xml
+parse xmlStr =
+    case XmlParser.parse xmlStr of
+        Ok xml ->
+            xml
+
+        Err _ ->
+            Debug.crash "Should not happen"
 
 
 main : BenchmarkProgram
